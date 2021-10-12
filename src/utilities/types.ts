2d47f223082a32eb0cc6bcf13f48659636d6e8b8
Cory Accardo <accardo@usc.edu>
2021-10-12 15:11:43 -0700

    added comment

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -26,6 +26,12 @@ export namespace Checkout{
         quantity: number
     }
 
+    /**
+     * A user defined type guard for a Product interface
+     * @param object - The object to be verified
+     * @returns true/false as to whether the object confomrs to Product interface.
+     */
+
     export const isProduct = (object : any) => {
         return "productId" in object && typeof object.productId === 'string'
             && "quantity" in object && typeof object.quantity === 'number'
@@ -42,6 +48,12 @@ export namespace Checkout{
         products : Product[]
     }
 
+    /**
+     * A user defined type guard for a Request interface
+     * @param object - The object to be verified
+     * @returns true/false as to whether the client request body conforms to the Request interface
+     */
+
     export const isRequest = (object : any) => {
         return "business_id" in object && typeof object.business_id === 'number'
             && "products" in object && object.products.every( (product : any) => isProduct(product));
@@ -52,13 +64,31 @@ export namespace Checkout{
 
 export namespace Row {
 
+    /**
+     * A generic type that ensures the object is one of the Rows defined in the namespace Row
+     */
+
     export type Generic = Business | StripeKeys
+
+    /**
+     * An object representation of a business row
+     * @member business_id - Refers to the unique primary key of the business
+     * @member stripe_pk - Refers the the stripe public key found in the stripe dashboard
+     * @member business_name - Refers to the name of the business
+
+     */
 
     export interface Business {
         business_id : number
         stripe_pk : string
         business_name : string
     }
+
+    /**
+     * A user defined type guard for a Business interface
+     * @param object - The object to be verified
+     * @returns true/false as to whether the object conforms to the Business interface
+     */
 
     export const isBusiness = (object: any) => {
         return "business_id" in object && typeof object.business_id === 'number'
@@ -67,10 +97,22 @@ export namespace Row {
     
     }
 
+    /**
+     * An object representation of a stripe_keys row
+     * @member stripe_pk - Refers the the stripe public key found in the stripe dashboard
+     * @member stripe_sk - Refers the the stripe secret key found in the stripe dashboard
+     */
+
     export interface StripeKeys {
         stripe_pk : string
         stripe_sk : string
     }
+
+    /**
+     * A user defined type guard for a StripeKEys interface
+     * @param object - The object to be verified
+     * @returns true/false as to whether the object conforms to the StripeKeys interface
+     */
 
     export const isStripeKeys = (object: any): object is StripeKeys => {
         return "stripe_pk" in object && typeof object.stripe_pk === 'string'
